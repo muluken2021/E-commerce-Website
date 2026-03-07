3ba14ddf3098a68c3f348c143ba82edcf97eb1cb
mulukenkassaw2025 <mulukenkassaw2021@gmail.com>
2026-03-07 11:45:46 +0300

    update

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,4 @@
 
-import hero_fashion from   "./image/products/hero_fashion.png"
-import hero_fashion2 from   "./image/products/hero_fashion2.png"
-import hero_beauty from   "./image/products/hero_beauty.png"
-import hero_grocery from   "./image/products/hero_grocery.png"
-import hero_automotivi from   "./image/products/hero_automotivi.png"
 import empitycart from   "./image/other/empitycart.png"
 
 
@@ -11,11 +6,7 @@ import empitycart from   "./image/other/empitycart.png"
 
 
 export  const assets = {
-   hero_fashion,
-   hero_fashion2,
-   hero_beauty,
-   hero_grocery,
-   hero_automotivi,
+
    empitycart
    
    
